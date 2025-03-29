0dd565f0ee9c55a06031b65c162c0ea25916c87e
bassem <bassembeshay50@gmail.com>
2025-03-29 17:55:44 +0200

    final

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -60,9 +60,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # قاعدة البيانات
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
