91fa991aa90b55deab7217395b5ddd9900e9843f
bassem <bassembeshay50@gmail.com>
2025-04-05 14:58:12 +0200

    finish edit

--- super/models.py
+++ super/models.py
@@ -1,94 +1,16 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
+class Suber(models.Model):
+    admin_id = models.BigAutoField(primary_key=True)
+    fname = models.CharField(max_length=50)
+    password = models.BigIntegerField()
 
     class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class Category(models.Model):
-    category_id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    imgc = models.ImageField(upload_to='category_images' , blank=True, null=True)
-    admin = models.ForeignKey('Suber', models.DO_NOTHING)
-
-    class Meta:
-        
-        db_table = 'category'
+        db_table = 'suber'
 
     def __str__(self):
-        return self.name     
+        return self.fname
 
 
 class Client(models.Model):
@@ -100,122 +22,75 @@ class Client(models.Model):
     password = models.CharField(max_length=50)
 
     class Meta:
-        managed = False
         db_table = 'client'
 
     def __str__(self):
         return self.user_name
 
+
+class Category(models.Model):
+    category_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    imgc = models.ImageField(upload_to='category_images', blank=True, null=True)
+    admin = models.ForeignKey(Suber, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return self.name
+
+
 class Color(models.Model):
     color_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=25)
-    img = models.ImageField(upload_to='color_images' , blank=True, null=True)
-    admin = models.ForeignKey('Suber', models.DO_NOTHING)
+    img = models.ImageField(upload_to='color_images', blank=True, null=True)
+    admin = models.ForeignKey(Suber, on_delete=models.CASCADE)
 
     class Meta:
-        
         db_table = 'color'
 
     def __str__(self):
-        return self.name    
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
+        return self.name
 
 
 class Product(models.Model):
     product_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     desc = models.CharField(max_length=600)
-    img = models.ImageField(upload_to='product_images' , blank=True, null=True)
+    img = models.ImageField(upload_to='product_images', blank=True, null=True)
     price = models.BigIntegerField()
-    admin = models.ForeignKey('Suber', models.DO_NOTHING)
-    category = models.ForeignKey(Category, models.DO_NOTHING)
+    admin = models.ForeignKey(Suber, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     design = models.CharField(max_length=400, blank=True, null=True)
     detail = models.CharField(max_length=400, blank=True, null=True)
 
     class Meta:
-        
         db_table = 'product'
 
     def __str__(self):
         return self.name
-    
+
 
 class ProductColor(models.Model):
     pc_id = models.BigAutoField(primary_key=True)
-    product = models.ForeignKey(Product, models.DO_NOTHING)
-    color = models.ForeignKey(Color, models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
 
     class Meta:
-        managed = False
         db_table = 'product_color'
 
     def __str__(self):
-        return self.product.name + ' - ' + self.color.name
+        return f'{self.product.name} - {self.color.name}'
+
 
 class ProductUser(models.Model):
     pu_id = models.BigAutoField(primary_key=True)
-    product = models.ForeignKey(Product, models.DO_NOTHING)
-    user = models.ForeignKey(Client, models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Client, on_delete=models.CASCADE)
 
     class Meta:
-        managed = False
         db_table = 'product_user'
 
     def __str__(self):
-        return self.product.name + ' - ' + self.user.user_name
-
-class Suber(models.Model):
-    admin_id = models.BigAutoField(primary_key=True)
-    fname = models.CharField(max_length=50)
-    password = models.BigIntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'suber'
-
-    def __str__(self):
-        return self.fname
+        return f'{self.product.name} - {self.user.user_name}'
